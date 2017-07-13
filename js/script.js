3c6e018403c767d156a1fd3093d52de86907012c
Scott Kipp <scott.ecfx@gmail.com>
2017-07-13 23:08:53 +0100

    added some new JS functionality and CSS styling

--- js/script.js
+++ js/script.js
@@ -1,99 +1,167 @@
-window.onresize = function(){ 
-      location.reload(); 
-  }
+// Rectifies the issue of the nav-bar not taking its proper place on screen when the window is resized on a browser
+window.onresize = function() { 
+    location.reload(); 
+}
   
-  // Functions for the interactive Nav-Bar. Duplicated variables to avoid declaring globals
-  function expandNav() {
-      var i = document.getElementById("myNav");
-	  var x = document.body;
-	  var n = document.getElementById("ngView");
-	  var p = document.getElementById("logo");
-	  var h = document.getElementsByClassName("panel-heading");
-	  var b = document.getElementsByClassName("panel-body");
-	  var z;
-	  var y;
+// Functions for the interactive Nav-Bar. Duplicated variables to avoid declaring globals
+function expandNav() {
+    var nav = document.getElementById("myNav");
+	var body = document.body;
+	var ng = document.getElementById("ngView");
+	var logo = document.getElementById("logo");
+	var panHead = document.getElementsByClassName("panel-heading");
+	var panBody = document.getElementsByClassName("panel-body");
+	var reg = document.getElementById("regForm");
+	var field = document.getElementsByTagName("input");
+	var selection = document.getElementsByTagName("select");
+	var z,y,x,v;
 	  
-	  if ($(window).width() > 750) {
-	      i.style.left = "0";
-	      x.style.backgroundColor = "rgb(153,153,153)";
-		  n.style.background = "rgb(153,153,153)";
-		  p.src = "pics/ReLife_logo2.jpg";
+	if ($(window).width() > 750) {
+	    nav.style.left = "0";
+	    body.style.backgroundColor = "rgb(153,153,153)";
+		ng.style.background = "rgb(153,153,153)";
+		logo.src = "pics/ReLife_logo2.jpg";
 		  
-		  for (z = 0; z < h.length; z++) {
-			  h[z].style.backgroundColor = "rgb(153,153,153)";
-		  }
+		// Included if statements due to clashes when trying to execute the code while not on the page that contained the affected classes
+		if (window.location.pathname == "/faq") {
 		  
-		  for (y = 0; y < b.length; y++) {
-			  b[y].style.backgroundColor = "rgb(153,153,153)";
-		  }
-      }
-  }
+		    for (z = 0; z < panHead.length; z++) {
+			    panHead[z].style.backgroundColor = "rgb(153,153,153)";
+		    }
+		  
+		    for (y = 0; y < panBody.length; y++) {
+			    panBody[y].style.backgroundColor = "rgb(153,153,153)";
+		    }
+		}
+		  
+		if (window.location.pathname == "/register") {
+		    reg.style.backgroundColor = "rgb(153,153,153)";
+		  
+		    for (x = 0; x < field.length; x++) {
+			    field[x].style.backgroundColor = "rgb(153,153,153)";
+		    }
+		  
+		    for (v = 0; v < selection.length; v++) {
+			    selection[v].style.backgroundColor = "rgb(153,153,153)";
+		    }
+		}
+    }
+}
   
-  function contractNav() {
-      var i = document.getElementById("myNav");
-	  var x = document.body;
-	  var n = document.getElementById("ngView");
-	  var p = document.getElementById("logo");
-	  var h = document.getElementsByClassName("panel-heading");
-	  var b = document.getElementsByClassName("panel-body");
-	  var z;
-	  var y;
+function contractNav() {
+    var nav = document.getElementById("myNav");
+	var body = document.body;
+	var ng = document.getElementById("ngView");
+	var logo = document.getElementById("logo");
+	var panHead = document.getElementsByClassName("panel-heading");
+	var panBody = document.getElementsByClassName("panel-body");
+	var reg = document.getElementById("regForm");
+	var field = document.getElementsByTagName("input");
+	var selection = document.getElementsByTagName("select");
+	var z,y,x,v;
 	  
-	  if ($(window).width() > 750) {
-	      i.style.left = "-175px";
-	      x.style.backgroundColor = "#fff";
-		  n.style.background = "linear-gradient(to bottom, rgba(155,235,255,0), rgba(155,235,255,1))";
-		  p.src = "pics/ReLife_logo.jpg";
+	if ($(window).width() > 750) {
+	    nav.style.left = "-175px";
+	    body.style.backgroundColor = "#fff";
+		ng.style.background = "linear-gradient(to bottom, rgba(155,235,255,0), rgba(155,235,255,1))";
+		logo.src = "pics/ReLife_logo.jpg";
 		  
-		  for (z = 0; z < h.length; z++) {
-			  h[z].style.backgroundColor = "#f5f5f5";
-		  }
+		if (window.location.pathname == "/faq") {
 		  
-		  for (y = 0; y < b.length; y++) {
-			  b[y].style.backgroundColor = "#fff";
-		  }
-      }
-  }
+		    for (z = 0; z < panHead.length; z++) {
+			    panHead[z].style.backgroundColor = "#f5f5f5";
+		    }
+		  
+		    for (y = 0; y < panBody.length; y++) {
+			    panBody[y].style.backgroundColor = "#fff";
+		    }
+		}
+		  
+		if (window.location.pathname == "/register") {
+		    reg.style.backgroundColor = "#fff";
+		  
+		    for (x = 0; x < field.length; x++) {
+			    field[x].style.backgroundColor = "#fff";
+		    }
+		  
+		    for (v = 0; v < selection.length; v++) {
+			    selection[v].style.backgroundColor = "#fff";
+		    }
+		}
+    }
+}
   
-  // The toggle function for touch screens that replaces the fade in and out version for desktop
-  function toggleNav() {
-      var i = document.getElementById("myNav");
-	  var x = document.body;
-	  var n = document.getElementById("ngView");
-	  var p = document.getElementById("logo");
-	  var h = document.getElementsByClassName("panel-heading");
-	  var b = document.getElementsByClassName("panel-body");
-	  var z;
-	  var y;
+// The toggle function for touch screens that replaces the animated mouseover version for desktop
+function toggleNav() {
+    var nav = document.getElementById("myNav");
+	var body = document.body;
+	var ng = document.getElementById("ngView");
+	var logo = document.getElementById("logo");
+	var panHead = document.getElementsByClassName("panel-heading");
+	var panBody = document.getElementsByClassName("panel-body");
+	var reg = document.getElementById("regForm");
+	var field = document.getElementsByTagName("input");
+	var selection = document.getElementsByTagName("select");
+	var z,y,x,v;  
 	  
-	  
-	  if ($(window).width() <= 750) {
-	      if (i.style.left == "-125px") {
-	          i.style.left = "0";
-	          x.style.backgroundColor = "rgb(153,153,153)";
-			  n.style.background = "rgb(153,153,153)";
-	          p.src = "pics/ReLife_logo2.jpg";
+	if ($(window).width() <= 750) {
+	    if (nav.style.left == "-125px") {
+	        nav.style.left = "0";
+	        body.style.backgroundColor = "rgb(153,153,153)";
+			ng.style.background = "rgb(153,153,153)";
+	        logo.src = "pics/ReLife_logo2.jpg";
 			  
-			  for (z = 0; z < h.length; z++) {
-				  h[z].style.backgroundColor = "rgb(153,153,153)";
-			  }
+			if (window.location.pathname == "/faq") {  
 			  
-			  for (y = 0; y < b.length; y++) {
-				  b[y].style.backgroundColor = "rgb(153,153,153)";
-			  }
-	      } else {
-	          i.style.left = "-125px";
-		      x.style.backgroundColor = "#fff";
-			  n.style.background = "linear-gradient(to bottom, rgba(155,235,255,0), rgba(155,235,255,1))";
-		      p.src = "pics/ReLife_logo.jpg";
+			    for (z = 0; z < panHead.length; z++) {
+				    panHead[z].style.backgroundColor = "rgb(153,153,153)";
+			    }
 			  
-			  for (z = 0; z < h.length; z++) {
-				  h[z].style.backgroundColor = "#f5f5f5";
-			  }
+			    for (y = 0; y < panBody.length; y++) {
+				    panBody[y].style.backgroundColor = "rgb(153,153,153)";
+			    }
+			}
 			  
-			  for (y = 0; y < b.length; y++) {
-				  b[y].style.backgroundColor = "#fff";
-			  }
-	      }
-	  }
-  }
+			if (window.location.pathname == "/register") {
+			    reg.style.backgroundColor = "rgb(153,153,153)";
+			  
+			    for (x = 0; x < field.length; x++) {
+				    field[x].style.backgroundColor = "rgb(153,153,153)";
+			    }
+			  
+			    for (v = 0; v < selection.length; v++) {
+				    selection[v].style.backgroundColor = "rgb(153,153,153)";
+			    }
+			}
+			  
+		} else {
+	        nav.style.left = "-125px";
+		    body.style.backgroundColor = "#fff";
+			ng.style.background = "linear-gradient(to bottom, rgba(155,235,255,0), rgba(155,235,255,1))";
+		    logo.src = "pics/ReLife_logo.jpg";
+			  
+			if (window.location.pathname == "/faq") {
+			  
+			    for (z = 0; z < panHead.length; z++) {
+				    panHead[z].style.backgroundColor = "#f5f5f5";
+			    }
+			  
+			    for (y = 0; y < panBody.length; y++) {
+				    panBody[y].style.backgroundColor = "#fff";
+			    }
+			}
+			  
+			if (window.location.pathname == "/register") {
+			    reg.style.backgroundColor = "rgb(153,153,153)";
+			  
+			    for (x = 0; x < field.length; x++) {
+				    field[x].style.backgroundColor = "#fff";
+			    }
+			  
+			    for (v = 0; v < selection.length; v++) {
+				    selection[v].style.backgroundColor = "#fff";
+			    }
+			}
+	    }
+	}
+}
